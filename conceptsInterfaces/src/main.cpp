f899de683b48f0d1aabd53a7e908e7d8d5a932d3
Gianluca Delfino <gianluca.delfino@gmail.com>
2019-07-13 22:43:10 +0100

    Fixed concept-based interfaces based on reddit suggestions

--- conceptsInterfaces/src/main.cpp
+++ conceptsInterfaces/src/main.cpp
@@ -1,46 +1,37 @@
-template <typename T>
-concept Shape = requires(const T& t)
+template <typename T> concept Shape = requires(const T& t)
 {
-    { t.area() } ->float;
-};
-
-struct Rectangle
-{
-    Rectangle()
     {
-        static_assert(Shape<decltype(*this)>);
+        t.area()
     }
-    float area() const;
+    ->float;
 };
 
-template <typename T>
-struct Square
+template <class T> struct ShapeBase
 {
-    Square()
-    {
-        static_assert(Shape<decltype(*this)>);
-    }
-    float area() const;
-    T edge;
+    ShapeBase() requires(Shape<T>) = default;
 };
 
-template <typename T>
-struct ShapeBase
-{
-    ShapeBase() { static_assert(Shape<T>); }
-};
-
-template <typename T>
-struct Circle : ShapeBase<Circle<T>>
+template <typename T> struct Circle : ShapeBase<Circle<T>>
 {
     float area() const;
     T radius;
 };
 
+template <class T> struct Square
+{
+    Square() requires(Shape<Circle<T>>) = default;
+    float area() const;
+    int edge;
+};
+
+#include <type_traits>
+static_assert(std::is_trivially_default_constructible_v<Circle<int>>);
+static_assert(std::is_trivially_default_constructible_v<Square<int>>);
+
 int main()
 {
-    Rectangle r;
-    Square<int> s;
-    Circle<float> c;
+    [[maybe_unused]] Square<int> s;
+    [[maybe_unused]] Circle<float> c;
+
     return 0;
 }
